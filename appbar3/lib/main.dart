d271eca3745ef660146cb4338b099baa4439cfd7
antoniobenitez01 <abenrod@g.educaand.es>
2025-12-12 16:59:02 +0100

    Finished AppBar 3 Exercise

--- appbar3/lib/main.dart
+++ appbar3/lib/main.dart
@@ -38,11 +38,44 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void _resetCounter(){
+    setState((){
+      _counter = 0;
+    });
+  }
+
+  Future<void> _showDialog() async{
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context){
+        return AlertDialog(
+          title: const Text('WARNING !'),
+          content: const SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text("Have a wonderful day!")
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text("OK!"),
+              onPressed: (){
+                Navigator.of(context).pop();
+              }
+            )
+          ]
+        );
+      }
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("AppBar 3"),
+        title: const Text("AppBar - Exercise 3"),
         
         //<Widget>[]
         actions: <Widget>[
@@ -51,14 +84,29 @@ class _MyHomePageState extends State<MyHomePage> {
           IconButton(
             icon: const Icon(Icons.comment),
             tooltip: 'Comment Icon',
-            onPressed: () {},
+            onPressed: () {
+              showModalBottomSheet<void>(
+                context: context, 
+                builder: (BuildContext context){
+                  return Container(
+                    height: 100,
+                    color : const Color.fromARGB(255, 7, 143, 255),
+                    child: Center(
+                      child: Text("YOU'VE CLICKED THE COMMENT BUTTON!",
+                      style:TextStyle(color:Colors.white))
+                    )
+                  );
+                });
+            },
           ), 
           
           //IconButton
           IconButton(
-            icon: const Icon(Icons.settings),
-            tooltip: 'Setting Icon',
-            onPressed: () {},
+            icon: const Icon(Icons.loop),
+            tooltip: 'Reset Button',
+            onPressed: () {
+              _resetCounter();
+            },
           ), 
         ], 
         backgroundColor: const Color.fromARGB(255, 0, 230, 219),
@@ -67,7 +115,9 @@ class _MyHomePageState extends State<MyHomePage> {
         leading: IconButton(
           icon: const Icon(Icons.menu),
           tooltip: 'Menu Icon',
-          onPressed: () {},
+          onPressed: () {
+            _showDialog();
+          },
         ),
         systemOverlayStyle: .light,
       ),
