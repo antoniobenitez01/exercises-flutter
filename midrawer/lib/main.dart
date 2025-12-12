8c0b2f93d01fcd0ceee6e911700a84a36f2ca53e
antoniobenitez01 <abenrod@g.educaand.es>
2025-12-12 19:40:44 +0100

    Finished My Drawer Exercise

--- midrawer/lib/main.dart
+++ midrawer/lib/main.dart
@@ -10,11 +10,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      title: 'My Drawer',
+      home: const MyHomePage(title: 'My Drawer Home Page'),
     );
   }
 }
@@ -29,37 +27,146 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        backgroundColor: Colors.blue,
+        title: Text(widget.title, style: TextStyle(color: Colors.white)),
+        iconTheme: IconThemeData(
+          color: Colors.white
+        ),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: .center,
+      drawer: Drawer(
+        backgroundColor: Colors.blue,
+        child: ListView(
+          padding: EdgeInsets.only(left:20),
           children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Container(
+              alignment: .topLeft,
+              padding: EdgeInsets.only(top: 60,left:10),
+              height:230,
+              color: Colors.blue,
+              child: Row(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        margin:EdgeInsets.only(bottom:10),
+                        child:CircleAvatar(
+                          radius: 40,
+                          backgroundColor: Colors.white,
+                          child: Text(
+                            "A",
+                            style: TextStyle(fontSize: 40, color: Colors.blue)
+                          )
+                        ),
+                      ),
+                      Text(
+                        "Antonio Benítez", 
+                        style: TextStyle(
+                          fontWeight: .bold,
+                          fontSize: 25,
+                          color: Colors.white
+                        )
+                      ),
+                      Text(
+                        "abenrod@g.educaand.es", 
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 14,
+                        )
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin:EdgeInsets.only(top:50),
+              height:50,
+              child: ListTile(
+                leading: const Icon(Icons.person, color: Colors.white),
+                title: const Text(
+                  "Profile",
+                  style:TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  )
+                ),
+                onTap: () {
+                  Navigator.pop(context);
+                }
+              )
+            ),
+            Container(
+              margin:EdgeInsets.zero,
+              child: ListTile(
+                leading: const Icon(Icons.message, color: Colors.white),
+                title: const Text(
+                  "Messages",
+                  style:TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  )
+                ),
+                onTap: () {
+                  Navigator.pop(context);
+                }
+              )
+            ),
+            Container(
+              margin:EdgeInsets.zero,
+              child: ListTile(
+                leading: const Icon(Icons.bookmark, color: Colors.white),
+                title: const Text(
+                  "Bookmarks",
+                  style:TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  )
+                ),
+                onTap: () {
+                  Navigator.pop(context);
+                }
+              )
+            ),
+            Container(
+              margin:EdgeInsets.zero,
+              child: ListTile(
+                leading: const Icon(Icons.settings, color: Colors.white),
+                title: const Text(
+                  "Settings",
+                  style:TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  )
+                ),
+                onTap: () {
+                  Navigator.pop(context);
+                }
+              )
+            ),
+            Container(
+              margin:EdgeInsets.zero,
+              child: ListTile(
+                leading: const Icon(Icons.logout, color: Colors.white),
+                title: const Text(
+                  "Logout",
+                  style:TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  )
+                ),
+                onTap: () {
+                  Navigator.pop(context);
+                }
+              )
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
