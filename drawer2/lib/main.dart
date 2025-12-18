cec8c8fae47c6c67adaf87a73b499e43de3b366d
antoniobenitez01 <anthonybenitezcontact@gmail.com>
2025-12-18 19:19:06 +0100

    Finished Drawer 2

--- drawer2/lib/main.dart
+++ drawer2/lib/main.dart
@@ -10,56 +10,117 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      title: 'Drawer 2',
+      home: const MainApp(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class MainApp extends StatefulWidget {
+  const MainApp({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MainApp> createState() => _MainAppState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MainAppState extends State<MainApp> {
 
-  void _incrementCounter() {
+  Widget _selectedWidget = Center(
+    child: Text("Selecciona un Widget")
+    );
+
+  void _updateWidget(Widget widget){
     setState(() {
-      _counter++;
+      _selectedWidget = widget;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title : Text("Widget Básicos en Flutter"),
+          actions : [
+            IconButton(
+              onPressed : () {
+              },
+              icon: Icon(Icons.settings),
+            )
+          ]
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        body: _selectedWidget,
+        drawer : Drawer(
+          child: ListView(
+            children: [
+              DrawerHeader(child: Text("Drawer Header")),
+              ListTile(
+                title : Text("Texto"),
+                subtitle : Text("Ejemplo de Texto"),
+                leading : Icon(Icons.text_fields),
+                trailing : Icon(Icons.arrow_forward),
+                onTap: () {
+                  _updateWidget(
+                    Center(
+                      child: Text(
+                        "Ejemplo de Texto !",
+                        style: TextStyle(
+                          color: Colors.red, 
+                          fontSize: 30,
+                          fontWeight: .bold
+                        )
+                      )
+                    )
+                  );
+                },
+              ),
+              ListTile(
+                title : Text("Botón"),
+                subtitle : Text("Ejemplo de Botón"),
+                leading : Icon(Icons.smart_button),
+                trailing : Icon(Icons.arrow_forward),
+                onTap: () {
+                  _updateWidget(
+                    Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          IconButton(
+                            onPressed : () {
+                              showModalBottomSheet<void>(
+                                context : context,
+                                builder : (BuildContext context) {
+                                  return Container(
+                                    height: 200,
+                                    color: Colors.amber,
+                                    child : Center(
+                                      child: Text("You pressed the Button !")
+                                    )
+                                  );
+                                }
+                              );
+                            },
+                            icon: Icon(Icons.settings),
+                            iconSize: 100,
+                          ),
+                          Text(
+                            "Press me!",
+                            style : TextStyle(
+                              fontSize: 50
+                            )
+                          )
+                        ]
+                      )
+                    ),
+                  );
+                },
+              ),
+            ]
+          )
+        ),
       ),
     );
   }
