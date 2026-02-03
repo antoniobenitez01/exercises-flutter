249ad5497678d0d0e33a93324fcb654e379ebe7d
antoniobenitez01 <anthonybenitezcontact@gmail.com>
2026-02-03 19:50:58 +0100

    Finished Practice 6 - Exercise 10

---
+++ exercises_practice6/ejercicio10/lib/joke.dart
@@ -0,0 +1,36 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class Joke {
+  Joke({
+    required this.type,
+    required this.setup,
+    required this.punchline,
+    required this.id,
+  });
+  
+  factory Joke.fromJson(Map<String, Object?> json) {
+    return Joke(
+      type: json['type']! as String,
+      setup: json['setup']! as String,
+      punchline: json['punchline']! as String,
+      id: json['id']! as int,
+    );
+  }
+  
+  final String type;
+  final String setup;
+  final String punchline;
+  final int id;
+}
+
+final dio = Dio();
+
+final randomJokeProvider = FutureProvider<Joke>((ref) async {
+  final response = await dio.get<Map<String, Object?>>(
+    'https://official-joke-api.appspot.com/random_joke',
+  );
+
+  return Joke.fromJson(response.data!);
+});
+  
